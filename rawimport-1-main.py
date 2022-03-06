3545c2d82f32ac6e3b5a1103f35688c4658c6820
aedra-v <85135938+aedra-v@users.noreply.github.com>
2022-03-06 12:43:36 -0600

    Create rawimport-1-main.py

---
+++ rawimport-1-main.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from google.cloud import storage
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import requests
+import json
+import sys
+import timeit
+
+headers = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'POST',
+	'Access-Control-Max-Age': '1000'
+}
+
+firebase_admin.initialize_app()
+
+def incominghttp(request):
+	fields = {}
+	data = request.form.to_dict()
+	for field in data:
+		fields[field] = data[field]
+		print('Processed field: %s' % field)
+	packagejson = json.dumps(fields, indent = 4)
+	json_object = json.loads(packagejson)
+	importintel = firestore.client().collection('intel-dump')
+	intelligence = importintel.document()
+	intelligence.set(json_object)
+
+	return (jsonify(success='true',), 200, headers)
