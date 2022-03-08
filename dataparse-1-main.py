31ba637ed4ccfdda710906fba2ee68a70698129f
aedra-v <85135938+aedra-v@users.noreply.github.com>
2022-03-08 06:51:53 -0600

    Update dataparse-1-main.py

--- dataparse-1-main.py
+++ dataparse-1-main.py
@@ -16,16 +16,17 @@ firebase_admin.initialize_app()
 
 def firestore_delta(event, context):
      resource_string = context.resource
-     
-     a1 = str(event()[5])
+     event_string = event.resource
+     importurl = event_string.items()[5]
+
+     a1 = str(importurl)
+     print(a1)
      a2 = a1.split("/")
      v1 = a2[-1]
      print(v1)
-     
-     json_object = json.dumps(event["value"], indent = 4)
-     json_string = json.loads(json_object)
+
      importintel = firestore.client().collection('d-prov')
      intelligence = importintel.document()
-     intelligence.set(json_string)
+     intelligence.set(event)
 
      return f'Success'
