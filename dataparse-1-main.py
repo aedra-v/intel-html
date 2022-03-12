33f0a8b1a625306708bb40cdd9eb5d630576fffb
aedra-v <85135938+aedra-v@users.noreply.github.com>
2022-03-11 23:19:54 -0600

    Update dataparse-1-main.py

--- dataparse-1-main.py
+++ dataparse-1-main.py
@@ -1,15 +1,7 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
 from google.cloud import storage
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import requests
-import json
-import sys
-import timeit
-from datetime import datetime
 import pandas as pd
 
 firebase_admin.initialize_app()
