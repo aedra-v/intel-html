0ff7fcbe12995b4121abaeae92c0a0ecd90230cb
aedra-v <85135938+aedra-v@users.noreply.github.com>
2022-03-11 23:28:47 -0600

    Update dataparse-throne-main.py

--- dataparse-throne-main.py
+++ dataparse-throne-main.py
@@ -11,39 +11,39 @@ def parse_throne(event, context):
     print('p1 pass')
 
     inteltable = pd.read_html(importhtml)
-    test2 = inteltable[0][0].iloc[1]: inteltable[0][1].iloc[1]
+    test2 = inteltable[0][0].iloc[1]
     print(test2)
 
     soup = BeautifulSoup(importhtml, 'html.parser')
     test3 = soup.find_all('h2')[2].a.get_text()
     print(test3)
 
+    importdict = {
+        'utotime': soup.find_all('h2')[2].br.next_element.split('(')[0].strip(),
+        'nexttick': soup.find_all('h2')[2].br.next_element.split('(')[1].strip().split(':')[1][:-1].strip(),
+        'provlabel': soup.find_all('h2')[2].next_element[16:-2],
+        'loc': soup.find_all('h2')[2].a.get_text(),
+        'thronecommand': soup.find('div', id = 'throne-monarch-message').p,
+        inteltable[0][0].iloc[1]: inteltable[0][1].iloc[1],
+        inteltable[0][0].iloc[2]: inteltable[0][1].iloc[2],
+        inteltable[0][0].iloc[3]: inteltable[0][1].iloc[3],
+        inteltable[0][0].iloc[4]: inteltable[0][1].iloc[4],
+        inteltable[0][0].iloc[5]: inteltable[0][1].iloc[5],
+        inteltable[0][0].iloc[6]: inteltable[0][1].iloc[6],
+        inteltable[0][0].iloc[7]: inteltable[0][1].iloc[7],
+        inteltable[0][0].iloc[8]: inteltable[0][1].iloc[8],
+        inteltable[0][0].iloc[9]: inteltable[0][1].iloc[9],
+        inteltable[0][2].iloc[1]: inteltable[0][1].iloc[1],
+        inteltable[0][2].iloc[2]: inteltable[0][3].iloc[2],
+        inteltable[0][2].iloc[3]: inteltable[0][3].iloc[3],
+        inteltable[0][2].iloc[4]: inteltable[0][3].iloc[4],
+        inteltable[0][2].iloc[5]: inteltable[0][3].iloc[5],
+        inteltable[0][2].iloc[6]: inteltable[0][3].iloc[6],
+        inteltable[0][2].iloc[7]: inteltable[0][3].iloc[7],
+        inteltable[0][2].iloc[8]: inteltable[0][3].iloc[8],
+        inteltable[0][2].iloc[9]: inteltable[0][3].iloc[9],
+    }
+
+    print(importdict)
+    
     return f'Success'
-
-importdict = {
-    'utotime': soup.find_all('h2')[2].br.next_element.split('(')[0].strip(),
-    'nexttick': soup.find_all('h2')[2].br.next_element.split('(')[1].strip().split(':')[1][:-1].strip(),
-    'provlabel': soup.find_all('h2')[2].next_element[16:-2],
-    'loc': soup.find_all('h2')[2].a.get_text(),
-    'thronecommand': soup.find('div', id = 'throne-monarch-message').p,
-    inteltable[0][0].iloc[1]: inteltable[0][1].iloc[1],
-    inteltable[0][0].iloc[2]: inteltable[0][1].iloc[2],
-    inteltable[0][0].iloc[3]: inteltable[0][1].iloc[3],
-    inteltable[0][0].iloc[4]: inteltable[0][1].iloc[4],
-    inteltable[0][0].iloc[5]: inteltable[0][1].iloc[5],
-    inteltable[0][0].iloc[6]: inteltable[0][1].iloc[6],
-    inteltable[0][0].iloc[7]: inteltable[0][1].iloc[7],
-    inteltable[0][0].iloc[8]: inteltable[0][1].iloc[8],
-    inteltable[0][0].iloc[9]: inteltable[0][1].iloc[9],
-    inteltable[0][2].iloc[1]: inteltable[0][1].iloc[1],
-    inteltable[0][2].iloc[2]: inteltable[0][3].iloc[2],
-    inteltable[0][2].iloc[3]: inteltable[0][3].iloc[3],
-    inteltable[0][2].iloc[4]: inteltable[0][3].iloc[4],
-    inteltable[0][2].iloc[5]: inteltable[0][3].iloc[5],
-    inteltable[0][2].iloc[6]: inteltable[0][3].iloc[6],
-    inteltable[0][2].iloc[7]: inteltable[0][3].iloc[7],
-    inteltable[0][2].iloc[8]: inteltable[0][3].iloc[8],
-    inteltable[0][2].iloc[9]: inteltable[0][3].iloc[9],
-}
-
-print(importdict)
